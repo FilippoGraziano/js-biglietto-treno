2e93cbe50c106b123d993eb4fcd925dafd27bf3b
Filippo Graziano <filippograziano13@gmail.com>
2026-05-11 12:35:46 +0200

    formatted script

--- script.js
+++ script.js
@@ -2,31 +2,26 @@ const age = prompt(`Quanti anni hai?`);
 const km = prompt(`Quanti km devi percorrere?`);
 
 const priceForKm = 0.21;
-const importo = parseInt(km) * priceForKm;
-const totalPrice = importo.toLocaleString('it-IT', {
+
+let importo = parseInt(km) * priceForKm;
+let totalPrice = 0
+
+//r calcolo sconto
+if (age < 18) {
+    importo = importo - (20 / 100 * importo)
+}
+else if (age > 65) {
+    importo = importo - (40 / 100 * importo)
+};
+
+totalPrice = importo;
+
+//r   € value
+totalPrice = importo.toLocaleString('it-IT', {
     style: 'currency',
     currency: 'EUR',
 });
 
-if (age < 18) {
-    const totalImport = importo - (20 / 100 * importo)
-    const totalPrice = totalImport.toLocaleString('it-IT', {
-        style: 'currency',
-        currency: 'EUR',
-    });
-    console.log(`${totalPrice}`);
-    document.getElementById(`prezzo-biglietti`).innerHTML = `${totalPrice}`;
-}
-else if (age > 65) {
-    const totalImport = importo - (40 / 100 * importo)
-    const totalPrice = totalImport.toLocaleString('it-IT', {
-        style: 'currency',
-        currency: 'EUR',
-    });
-    console.log(`${totalPrice}`);
-    document.getElementById(`prezzo-biglietti`).innerHTML = `${totalPrice}`;
-}
-else {
-    console.log(`${totalPrice}`)
-    document.getElementById(`prezzo-biglietti`).innerHTML = `${totalPrice}`
-};
+//r display in console and html
+console.log(totalPrice);
+document.getElementById(`prezzo-biglietti`).innerHTML = `${totalPrice}`;
